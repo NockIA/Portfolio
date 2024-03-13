e8882f604efeb24aa41b2ad5c296d67c19833750
ckevin <ISeeNock.Draw@gmail.com>
2024-03-13 19:49:16 +0100

    :spakles: | finish about section

--- Portfolio/src/components/about/about.tsx
+++ Portfolio/src/components/about/about.tsx
@@ -4,26 +4,28 @@ import { AboutProps } from "../../utils/about_properties";
 
 const AboutComponent: React.FC<AboutProps> = ({ name, description }) => {
   return (
-    <section id="#about" className="row-container container-about-page">
-      <article className="column-container container-informations">
-        <h1>{name}</h1>
-        <div className="column-container container-descriptions">
-          {description.map((desc, index) => (
-            <p key={index}>{desc}</p>
-          ))}
+    <section className="container-global-about">
+      <section id="#about" className="row-container container-about-page">
+        <article className="column-container container-informations">
+          <h1>{name}</h1>
+          <div className="column-container container-descriptions">
+            {description.map((desc, index) => (
+              <p key={index}>{desc}</p>
+            ))}
+          </div>
+          <a href="#contacts" className="row-container contact-button">
+            <p className="contact-link">{"Contactez-moi"}</p>
+            <img src="./src/assets/images/arrow_right.png" />
+          </a>
+        </article>
+        <div className="container-profile-picture">
+          <img
+            className="profile-picture"
+            src="./src/assets/images/profile_picture.png"
+            alt="profile_picture"
+          />
         </div>
-        <a href="#contacts" className="row-container contact-button">
-          <p className="contact-link">{"Contactez-moi"}</p>
-          <img src="./src/assets/images/arrow_right.png" />
-        </a>
-      </article>
-      <div className="container-profile-picture">
-        <img
-          className="profile-picture"
-          src="./src/assets/images/profile_picture.png"
-          alt="profile_picture"
-        />
-      </div>
+      </section>
     </section>
   );
 };
--- Portfolio/src/views/home.tsx
+++ Portfolio/src/views/home.tsx
@@ -1,15 +1,24 @@
 import AboutComponent from "../components/about/about";
 import NavigationBar from "../components/navigation";
 import { AboutDatas } from "../utils/about_properties";
+import "../styles/global.css";
+import "../styles/home.css";
 
 const HomePage = () => {
   return (
     <>
       <NavigationBar />
-      <AboutComponent
-        name={AboutDatas.name}
-        description={AboutDatas.description}
-      />
+      <main className="column-container container-home-page">
+        <AboutComponent
+          name={AboutDatas.name}
+          description={AboutDatas.description}
+        />
+        <img
+          className="down-chevron"
+          src="./src/assets/images/down_chevron.png"
+        />
+        <span className="separation" />
+      </main>
     </>
   );
 };
